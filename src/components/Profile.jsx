f7b2c1e50e942343aff3f00a6c423fb3b38ad17d
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-07-01 21:10:20 +0300

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 import css from './Profile.module.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
-  return <div></div>;
+  return (
+    <div className={css.profile}>
+      <div className={css.userinfo}>
+        <img src={avatar} alt={username} className={css.userPicture} />
+        <p className={css.userName}>{username}</p>
+        <p className={css.userTag}>@{tag}</p>
+        <p className={css.userLocation}>{location}</p>
+      </div>
+
+      <ul className={css.profileList}>
+        <li className={css.listElement}>
+          <span className={css.listElementText}>Followers</span>
+          <span className={css.listElementStats}>{stats.followers}</span>
+        </li>
+        <li className={css.listElement}>
+          <span className={css.listElementText}>Views</span>
+          <span className={css.listElementStats}>{stats.views}</span>
+        </li>
+        <li className={css.listElement}>
+          <span className={css.listElementText}>Likes</span>
+          <span className={css.listElementStats}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Profile;
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import css from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {};
