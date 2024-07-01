ff183a321393ac5a853f5d354e4ef7e6d814131d
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-07-01 18:17:41 +0300

    test

---
+++ src/components/Profile.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import css from './Profile.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return <div></div>;
+};
+
+export default Profile;
