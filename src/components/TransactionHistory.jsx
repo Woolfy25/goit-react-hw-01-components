5c99930801978160f0df6c73a39c098ea486a3a6
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-07-02 17:04:42 +0300

    Transaction History

---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import css from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody className={css.tableBody}>
+        {items.map(item => {
+          return (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
